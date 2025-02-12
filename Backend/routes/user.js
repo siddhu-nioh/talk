9922aaf62af22fc60bdd06cd80399a78b854e70e
BONAM HARI VENU NARASIMHA SIDDHU <121369671+siddhu-nioh@users.noreply.github.com>
2025-02-13 00:32:37 +0530

    Update user.js

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -22,6 +22,8 @@ router.post('/login', saveRedirectUrl, (req, res, next) => {
     })(req, res, next);
 });
 router.get("/auth/check", (req, res) => {
+    
+    console.log("User:", req.user);
     if (req.isAuthenticated()) {
         return res.json({ authenticated: true, user: req.user });
     } else {

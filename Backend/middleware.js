52d952be572dad6715b84555db8c03d4ab62e722
BONAM HARI VENU NARASIMHA SIDDHU <121369671+siddhu-nioh@users.noreply.github.com>
2025-02-12 23:48:42 +0530

    Update middleware.js
    some

--- Backend/middleware.js
+++ Backend/middleware.js
@@ -1,34 +1,37 @@
 module.exports.isLoggedIn = (req, res, next) => {
-      if (req.isAuthenticated && req.isAuthenticated()) {
-            return next();
-      }
-      req.flash('error', 'You must be logged in to access this page!');
-      return res.redirect('/login');
+    if (typeof req.isAuthenticated === "function" && req.isAuthenticated()) {
+        return next();
+    }
+    req.flash('error', 'You must be logged in to access this page!');
+    return res.redirect('/login');
 };
 
-
 module.exports.ensureAuthenticated = (req, res, next) => {
-      if (req.isAuthenticated()) {
-            res.locals.currUser = req.user;
-            return next();
-      }
-      req.flash('error', 'You must be signed in to do that!');
-      res.redirect('/login');
+    if (req.isAuthenticated()) {
+        res.locals.currUser = req.user;
+        return next();
+    }
+    if (req.headers.accept && req.headers.accept.includes("application/json")) {
+        return res.status(401).json({ message: "Unauthorized", authenticated: false });
+    }
+    req.flash('error', 'You must be signed in to do that!');
+    res.redirect('/login');
 };
 
 module.exports.validateMedia = (req, res, next) => {
-      const image = req.files?.image ? req.files.image[0].path : null;
-      const video = req.files?.video ? req.files.video[0].path : null;
-      if (!image && !video) {
-            req.flash("error", "Either an image or video must be provided.");
-            return res.redirect("/talk/new");
-      }
-      req.media = { image, video };
-      next();
+    const image = req.files?.image ? req.files.image[0].path : null;
+    const video = req.files?.video ? req.files.video[0].path : null;
+    if (!image && !video) {
+        req.flash("error", "Either an image or video must be provided.");
+        return res.redirect("/talk/new");
+    }
+    req.media = { image, video };
+    next();
 };
+
 module.exports.saveRedirectUrl = (req, res, next) => {
-      if (req.session.redirectUrl) {
-            res.locals.redirectUrl = req.session.redirectUrl;
-      }
-      next();
-}
+    if (req.session.redirectUrl) {
+        res.locals.redirectUrl = req.session.redirectUrl;
+    }
+    next();
+};

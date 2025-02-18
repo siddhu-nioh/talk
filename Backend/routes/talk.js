5fbe26f155a1f0d651631295b6df1c696068423e
BONAM HARI VENU NARASIMHA SIDDHU <121369671+siddhu-nioh@users.noreply.github.com>
2025-02-18 14:25:46 +0530

    Update talk.js

--- Backend/routes/talk.js
+++ Backend/routes/talk.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const upload = require('../cloudConfig');
 const talkRouter=require('../controllers/talk');
 const wrapAsync=require('../utils/wrapAsync');
-const {isLoggedIn,ensureAuthenticated,validateMedia}=require('../middleware')
+const {ensureAuthenticated,validateMedia}=require('../middleware')
 
 router.get('/', talkRouter.renderIndex);
 router.get('/new',ensureAuthenticated,talkRouter.renderNew);
 router.get('/search',ensureAuthenticated,talkRouter.renderSearch);
-router.get('/user',isLoggedIn,talkRouter.renderUser);
+router.get('/user',talkRouter.renderUser);
 router.post('/', upload,validateMedia, wrapAsync(talkRouter.postUpload));
 router.get('/search/users',wrapAsync(talkRouter.searchUsers));
 router.get('/user/:id',ensureAuthenticated,wrapAsync(talkRouter.showUsers));

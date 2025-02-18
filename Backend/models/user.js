c67c8e62999a1a5e72d46b0a25886db9bd18fe9d
BONAM HARI VENU NARASIMHA SIDDHU <121369671+siddhu-nioh@users.noreply.github.com>
2025-02-18 14:25:18 +0530

    Update user.js

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require("passport-local-mongoose");
+const bcrypt = require("bcryptjs"); // For password hashing
+const jwt = require("jsonwebtoken"); // For generating JWTs
 
 const userSchema = new Schema({
     username: {
@@ -19,13 +20,38 @@ const userSchema = new Schema({
         default: "https://res.cloudinary.com/dvykfqu85/image/upload/v1737733131/Talk_Test/mj2vlw57rmwvxmgky1ve.webp",
         set: (v) => v === "" ? "https://res.cloudinary.com/dvykfqu85/image/upload/v1737733131/Talk_Test/mj2vlw57rmwvxmgky1ve.webp" : v,
     },
+    password: {
+        type: String,
+        required: true, 
+    },
+    salt: {
+        type: String, 
+    },
     followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
     following: [{ type: Schema.Types.ObjectId, ref: "User" }],
     posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
 });
 
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        this.salt = await bcrypt.genSalt(10); // Generate salt
+        this.password = await bcrypt.hash(this.password, this.salt); // Hash password
+    }
+    next();
+});
 
-userSchema.plugin(passportLocalMongoose);
+// Generate JWT
+userSchema.methods.generateAuthToken = function () {
+    return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: "7d", // Token expires in 7 days
+    });
+};
+
+// Compare password
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;

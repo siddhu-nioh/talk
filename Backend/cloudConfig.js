1db9d4c0ad9d3ad3bb82f23e0d876b6ceaf00633
BONAM HARI VENU NARASIMHA SIDDHU <hvnsiddhu7@gmail.com>
2025-05-08 08:18:35 +0530

    Update cloudConfig.js

--- Backend/cloudConfig.js
+++ Backend/cloudConfig.js
@@ -30,27 +30,24 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET,
+      cloud_name: process.env.CLOUD_NAME,
+      api_key: process.env.CLOUD_API_KEY,
+      api_secret: process.env.CLOUD_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-        folder: 'Talk_Test',
-        allowed_formats: ['png', 'jpg', 'jpeg', 'webp', 'mp4', 'mov', 'avi'],
-        resource_type: 'auto',
-    },
+      cloudinary: cloudinary,
+      params: {
+            folder: 'Talk_Test',
+            allowed_formats: ['png', 'jpg', 'jpeg', 'webp', 'mp4', 'mov', 'avi'],
+            resource_type: 'auto',
+      },
 });
 
-// Create the multer middleware
 const upload = multer({ storage }).fields([
-    { name: 'image', maxCount: 1 },
-    { name: 'video', maxCount: 1 },
-    { name: 'profile', maxCount: 1 },
-    { name: 'profilePicture', maxCount: 1 }
+      { name: 'image', maxCount: 1 },
+      { name: 'video', maxCount: 1 },
+      { name: 'profile', maxCount: 1 },
 ]);
 
-// Export the middleware function
 module.exports = upload;

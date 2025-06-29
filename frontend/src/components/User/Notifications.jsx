0c42fb31736d26e2f27ee224b499396e08c86075
BONAM HARI VENU NARASIMHA SIDDHU <hvnsiddhu7@gmail.com>
2025-06-29 18:20:35 +0530

    Update Notifications.jsx

--- frontend/src/components/User/Notifications.jsx
+++ frontend/src/components/User/Notifications.jsx
@@ -1680,7 +1680,7 @@ const isOutgoing = msg.sender === currentUser?._id;
           gap: '10px',
           animation: 'fadeIn 0.5s',
         }}>
-          <button 
+          {/* <button 
             onClick={() => fileInputRef.current.click()}
             style={{
               background: 'none',
@@ -1707,15 +1707,15 @@ const isOutgoing = msg.sender === currentUser?._id;
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
               <path d="M4 16L8.586 11.414C8.96121 11.0391 9.46985 10.8284 10 10.8284C10.5302 10.8284 11.0388 11.0391 11.414 11.414L16 16M14 14L15.586 12.414C15.9612 12.0391 16.4698 11.8284 17 11.8284C17.5302 11.8284 18.0388 12.0391 18.414 12.414L20 14M14 8H14.01M6 20H18C18.5304 20 19.0391 19.7893 19.4142 19.4142C19.7893 19.0391 20 18.5304 20 18V6C20 5.46957 19.7893 4.96086 19.4142 4.58579C19.0391 4.21071 18.5304 4 18 4H6C5.46957 4 4.96086 4.21071 4.58579 4.58579C4.21071 4.96086 4 5.46957 4 6V18C4 18.5304 4.21071 19.0391 4.58579 19.4142C4.96086 19.7893 5.46957 20 6 20Z" stroke="#4F46E5" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
-          </button>
+          </button> */}
           
-          <input 
+          {/* <input 
             type="file" 
             ref={fileInputRef} 
             onChange={handleFileSelect} 
             style={{ display: 'none' }} 
             accept="image/*,video/*,audio/*,.pdf,.doc,.docx,.xls,.xlsx,.txt"
-          />
+          /> */}
           
           <div style={{
             flex: 1,
